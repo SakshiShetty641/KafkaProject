348bdd67a299e9efd92679ef854f59d5f7938e93
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-03-27 13:17:37 +0530

    To demonstrate Kafka Wikimedia Producer

---
+++ src/main/java/com/practice/wikimedia/WikiKafkaProducer.java
@@ -0,0 +1,36 @@
+package com.practice.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikiKafkaProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        String  bootstrapServers = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia";
+
+        EventHandler eventHandler = new WikiMediaHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url)); //Build an event source using the event hanlder
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+
+    }
+}
